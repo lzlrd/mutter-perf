09fc47ba79bd563bd564673cddfd535309e68553
Daniel van Vugt <daniel.van.vugt@canonical.com>
2021-12-07 18:31:16 +0800

    kms/impl-device/simple: Pump the CRTC's buffer reference queue on scanout
    In this case "on scanout" means when `drmModeSetCrtc` succeeds. This is
    the only case where there won't be a `meta_kms_page_flip_data_flipped` to
    do it for us.

--- src/backends/native/meta-kms-impl-device-simple.c
+++ src/backends/native/meta-kms-impl-device-simple.c
@@ -466,6 +466,8 @@ process_mode_set (MetaKmsImplDevice  *impl_device,
       return FALSE;
     }
 
+  meta_kms_crtc_on_scanout_started (crtc);
+
   if (drm_mode)
     {
       g_hash_table_replace (impl_device_simple->cached_mode_sets,
@@ -853,6 +855,8 @@ mode_set_fallback (MetaKmsImplDeviceSimple  *impl_device_simple,
                    g_strerror (-ret));
       return FALSE;
     }
+
+  meta_kms_crtc_on_scanout_started (crtc);
 
   if (!impl_device_simple->mode_set_fallback_feedback_source)
     {
